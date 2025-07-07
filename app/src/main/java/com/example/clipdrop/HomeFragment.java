871d898e11c8cf9fccbd2bc6b796c1abe8299378
Gurpreet Singh <parasrockz81@gmail.com>
2025-07-07 11:35:09 -0600

    Added two fragments one for showing file on server to download and other one to show history of shared files

--- app/src/main/java/com/example/clipdrop/HomeFragment.java
+++ app/src/main/java/com/example/clipdrop/HomeFragment.java
@@ -7,8 +7,11 @@ import android.os.Bundle;
 
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -17,6 +20,8 @@ import android.view.ViewGroup;
 
 public class HomeFragment extends Fragment {
 
+    ViewPager2 viewPager;
+    MyViewPagerAdapter viewPagerAdapter;
     private final ActivityResultLauncher<String> requestPermissionLauncher =
             registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
                 if (isGranted) {
@@ -46,5 +51,23 @@ public class HomeFragment extends Fragment {
         }
 
         return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        viewPagerAdapter = new MyViewPagerAdapter(
+                getActivity().getSupportFragmentManager(),
+                getLifecycle()
+        );
+
+        viewPagerAdapter.addFragment(new DownloadUploadFragment());
+        viewPagerAdapter.addFragment(new HistoryFragment());
+
+        viewPager = view.findViewById(R.id.viewPager);
+
+        viewPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+
+        viewPager.setAdapter(viewPagerAdapter);
     }
 }
---
+++ app/src/main/java/com/example/clipdrop/MyViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.clipdrop;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import java.util.ArrayList;
+
+public class MyViewPagerAdapter extends FragmentStateAdapter {
+
+    private ArrayList<Fragment> fragmentList = new ArrayList<>();
+
+    public MyViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle) {
+        super(fragmentManager, lifecycle);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        return fragmentList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+       return fragmentList.size();
+    }
+
+    public void addFragment(Fragment f) {
+        fragmentList.add(f);
+    }
+}
